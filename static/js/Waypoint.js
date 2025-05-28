a15236354ed1737e052ff384576aba9f9dfeac4c
Bruce Cooner <brucecooner@gmail.com>
2025-05-27 21:33:04 -0700

    -lines mode -> waypoints mode (rendering disabled now tho)

---
+++ static/js/Waypoint.js
@@ -0,0 +1,17 @@
+var WaypointNS = {
+	// there is no single specification about which dimension comes first, so I will reject
+	// either and demand an object, but allow for two key name sets on that object
+	Waypoint:function(lat, lng, elevation)
+	{
+		this.longitude = undefined;
+		this.latitude = undefined;
+		this.elevation = undefined;
+		
+		// more to come...
+
+		this.latitude = lat;
+		this.longitude = lng;
+
+		this.elevation = elevation;
+	}
+};
